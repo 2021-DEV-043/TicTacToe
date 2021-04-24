eb7f2a25b30b80b28d38b505a3ce834c644b1546
“2021-DEV-043” <dev0432021@gmail.com>
2021-04-24 16:57:59 +0530

    refactor: renamed default param names and extracted literals as constants for better readability

--- src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGame.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class TicTacToeGame {
 
-	private char[][] gameBoard = new char[3][3];
+	private static final int BOARD_SIZE = 3;
+	
+	private char[][] gameBoard = new char[BOARD_SIZE][BOARD_SIZE];
 
-	public void placeMoveOnTheBoard(int i, int j, char c) {
-		gameBoard[i][j] = c;
+	public void placeMoveOnTheBoard(int row, int column, char player) {
+		gameBoard[row][column] = player;
 	}
 
-	public char identifyPlayerAt(int i, int j) {
-		return gameBoard[i][j];
+	public char identifyPlayerAt(int row, int column) {
+		return gameBoard[row][column];
 	}
 
 }
