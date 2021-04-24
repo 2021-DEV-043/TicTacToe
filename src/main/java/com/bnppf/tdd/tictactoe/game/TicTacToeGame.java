c7ed84c2f56adcd545b64af65164419d0346b518
“2021-DEV-043” <dev0432021@gmail.com>
2021-04-24 17:56:56 +0530

    feat: should get next player based upon current player

--- src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGame.java
@@ -1,20 +1,27 @@
 package com.bnppf.tdd.tictactoe.game;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TicTacToeGame {
 
 	private static final int BOARD_SIZE = 3;
 	
 	private char[][] gameBoard = new char[BOARD_SIZE][BOARD_SIZE];
-
+	
 	public void placeMoveOnTheBoard(int row, int column, char player) {
 		gameBoard[row][column] = player;
 	}
 
 	public char identifyPlayerAt(int row, int column) {
 		return gameBoard[row][column];
+	}
+	
+	public char getNextPlayer(char currentPlayer) {
+		return currentPlayer == 'X' ? 'O' : 'X';
 	}
 
 }
--- src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameTest.java
+++ src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameTest.java
@@ -1,9 +1,11 @@
 package com.bnppf.tdd.tictactoe.game;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -12,13 +14,22 @@ public class TicTacToeGameTest {
 	private static final int POSITION_ONE = 1;
 	private static final char PLAYER_X = 'X';
 	
+	@Autowired
+	private TicTacToeGame game;
+	
 	@Test
 	@DisplayName("Game should allow player X to play at any position on the board")
 	public void playerXShouldBeAbleToMakeMoveInAnyPositionOnTheBoardAndIdentifyTheSame() {
-		TicTacToeGame game = new TicTacToeGame();
 		game.placeMoveOnTheBoard(POSITION_ONE, POSITION_ONE, PLAYER_X);
 
 		assertEquals(PLAYER_X, game.identifyPlayerAt(POSITION_ONE, POSITION_ONE));
+	}
+	
+	@Test
+	@DisplayName("Should get next player based upon current player")
+	public void shouldGetNextPlayerBasedUponCurrentPlayer() {
+		assertAll(() -> assertEquals(PLAYER_X, game.getNextPlayer('O')),
+				() -> assertEquals('O', game.getNextPlayer(PLAYER_X)));
 	}
 
 }
