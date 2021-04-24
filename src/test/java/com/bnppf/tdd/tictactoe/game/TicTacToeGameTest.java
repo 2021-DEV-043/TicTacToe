03fba6aba24712fb65e38dc4cae00d9eacd140a1
“2021-DEV-043” <dev0432021@gmail.com>
2021-04-24 16:58:58 +0530

    test: extracted string literals as constants for reusability

--- src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameTest.java
+++ src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameTest.java
@@ -9,13 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class TicTacToeGameTest {
 
+	private static final int POSITION_ONE = 1;
+	private static final char PLAYER_X = 'X';
+	
 	@Test
 	@DisplayName("Game should allow player X to play at any position on the board")
 	public void playerXShouldBeAbleToMakeMoveInAnyPositionOnTheBoardAndIdentifyTheSame() {
 		TicTacToeGame game = new TicTacToeGame();
-		game.placeMoveOnTheBoard(1, 1, 'X');
+		game.placeMoveOnTheBoard(POSITION_ONE, POSITION_ONE, PLAYER_X);
 
-		assertEquals('X', game.identifyPlayerAt(1, 1));
+		assertEquals(PLAYER_X, game.identifyPlayerAt(POSITION_ONE, POSITION_ONE));
 	}
 
 }
