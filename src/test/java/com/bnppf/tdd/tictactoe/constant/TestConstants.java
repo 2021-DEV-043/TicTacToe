2400b47b1bb4d50155f686f4ec1aea12f2cbc27b
“2021-DEV-043” <dev0432021@gmail.com>
2021-04-25 17:04:26 +0530

    test: extracted string literal as constant for better readability

--- src/test/java/com/bnppf/tdd/tictactoe/constant/TestConstants.java
+++ src/test/java/com/bnppf/tdd/tictactoe/constant/TestConstants.java
@@ -22,4 +22,6 @@ public final class TestConstants {
 	public static final String GAME_WINNER_PLAYERX_MESSAGE = "Winner of the game is Player 'X'";
 	public static final String GAME_WINNER_PLAYERO_MESSAGE = "Winner of the game is Player 'O'";
 	public static final String GAME_DRAW_MESSAGE = "It's a Draw Game";
+	
+	public static final String USER_INPUT_ONE = "1";
 }
--- src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutorTest.java
+++ src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutorTest.java
@@ -1,5 +1,6 @@
 package com.bnppf.tdd.tictactoe.game;
 
+import static com.bnppf.tdd.tictactoe.constant.TestConstants.USER_INPUT_ONE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +14,7 @@ public class TicTacToeGameExecutorTest {
 	@DisplayName("Declare user input as invalid if user provides input of length other than two")
 	public void declareUserInputasInvalidIfUserProvidesInputOfLengthOtherThanTwo() {
 		TicTacToeGameExecutor gameExecutor = new TicTacToeGameExecutor();
-		String[] userInputs = {"1", "1", "1"};
+		String[] userInputs = {USER_INPUT_ONE, USER_INPUT_ONE, USER_INPUT_ONE};
 		
 		assertTrue(gameExecutor.isUserInputInvalid(userInputs));
 	}
