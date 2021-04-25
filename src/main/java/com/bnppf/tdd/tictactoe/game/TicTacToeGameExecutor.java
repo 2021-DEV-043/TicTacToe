fd974458ce9946e567462cd29b6ae490b6c1a31a
“2021-DEV-043” <dev0432021@gmail.com>
2021-04-25 18:02:51 +0530

    feat: should declare a player as winner based upon the user inputs

--- src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutor.java
+++ src/main/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutor.java
@@ -1,21 +1,53 @@
 package com.bnppf.tdd.tictactoe.game;
 
+import static com.bnppf.tdd.tictactoe.constant.GameConstants.GAME_CONTINUE_MESSAGE;
 import static com.bnppf.tdd.tictactoe.constant.GameConstants.INDEX_ONE;
 import static com.bnppf.tdd.tictactoe.constant.GameConstants.INDEX_ZERO;
 import static com.bnppf.tdd.tictactoe.constant.GameConstants.INVALID_INPUT_FORMAT_EXCEPTION_MESSAGE;
 import static com.bnppf.tdd.tictactoe.constant.GameConstants.USER_INPUT_SIZE;
 import static com.bnppf.tdd.tictactoe.constant.GameConstants.VALID_PATTERN;
 
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.bnppf.tdd.tictactoe.exception.InvalidUserInputException;
+import com.bnppf.tdd.tictactoe.exception.PositionAlreadyOccupiedException;
+import com.bnppf.tdd.tictactoe.exception.PositionOutOfRangeException;
+import com.bnppf.tdd.tictactoe.model.Position;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TicTacToeGameExecutor {
 
+	@Autowired
+	private TicTacToeGame game;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TicTacToeGameExecutor.class);
+	
+	public String runGame() {
+		String result = "Game Started";
+		Scanner scan = new Scanner(System.in);
+
+		while (GAME_CONTINUE_MESSAGE.equals(result) || "Game Started".equals(result)) {
+			try {
+				String[] input = scan.nextLine().split(",");
+				validateUserInputs(input);
+				result = game.play(new Position(Integer.parseInt(input[INDEX_ZERO]), Integer.parseInt(input[INDEX_ONE])));
+			} catch (InvalidUserInputException | PositionOutOfRangeException | PositionAlreadyOccupiedException exception) {
+				String errorMessage = String.format("%s %s", "Error while playing game...!!", exception.getMessage());
+				LOGGER.error(errorMessage);
+			}
+		}
+		scan.close();
+
+		return result;
+	}
 	
 	public void validateUserInputs(String[] userInputs) throws InvalidUserInputException {
 		if (isUserInputInvalid(userInputs)) {
--- src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutorTest.java
+++ src/test/java/com/bnppf/tdd/tictactoe/game/TicTacToeGameExecutorTest.java
@@ -1,8 +1,12 @@
 package com.bnppf.tdd.tictactoe.game;
 
+import static com.bnppf.tdd.tictactoe.constant.TestConstants.GAME_WINNER_PLAYERX_MESSAGE;
 import static com.bnppf.tdd.tictactoe.constant.TestConstants.USER_INPUT_ONE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +42,16 @@ public class TicTacToeGameExecutorTest {
 	public void shouldThrowExceptionIfUserInputsAreInvalid() {
 		String[] userInputs = {USER_INPUT_ONE, ""};
 		assertThrows(InvalidUserInputException.class, () -> gameExecutor.validateUserInputs(userInputs));
+	}
+	
+	@Test
+	@DisplayName("Should declare a player as winner based upon user inputs")
+	public void shouldDeclarePlayerAsWinnerBasedUponUserInputsFromCommandLine() {
+		StringBuilder userInputBuilder = new StringBuilder("0,0").append("\n").append("1,1").append("\n")
+				.append("0,1").append("\n").append("2,1").append("\n").append("0,2");
+		System.setIn(new ByteArrayInputStream(userInputBuilder.toString().getBytes()));
+
+		assertEquals(GAME_WINNER_PLAYERX_MESSAGE, gameExecutor.runGame());
 	}
 
 }
